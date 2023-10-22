a80c4410a75f3e282bf2e3a8e5288bdf48640887
Dalton Dayton <daltondayton1@gmail.com>
2023-10-22 12:36:15 -0600

    add methods to class declaration

--- Stack.h
+++ Stack.h
@@ -1,15 +1,30 @@
+template <class Type>
 class Stack
 {
 private:
 public:
     Stack();
     ~Stack();
+    Stack();
+    Stack(const Stack &other);
+    Stack &operator=(const Stack &other);
+    int size() const;
+    bool empty() const;
+    Type top() const;
+    void push(const Type &);
+    void pop();
+    void pop(int n);
+    Type topPop();
+    void clear();
+    friend std::ostream &operator<< <>(std::ostream &, const Stack &stack);
 };
 
-Stack::Stack()
+template <class Type>
+Stack<Type>::Stack()
 {
 }
 
-Stack::~Stack()
+template <class Type>
+Stack<Type>::~Stack()
 {
 }
